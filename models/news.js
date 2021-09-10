168f3815ad9aed0c3a39542d794a9573acdf6b23
AndLD <nil10035@gmail.com>
2021-09-10 09:39:17 +0300

    console error

--- models/news.js
+++ models/news.js
@@ -1,4 +1,4 @@
-const mysql = require("../db/mysql")
+const mysql = require('../db/mysql')
 
 /* SQL Table:
 
@@ -15,64 +15,98 @@ docx
 
 exports.insertNews = (news) => {
     return new Promise((resolve) => {
-        var query = mysql.connection.query("INSERT INTO news (title, tags, addDate, html, docx) VALUES (?, ?, ?, ?, ?)",
-        [news.title, news.addDate, news.html, news.docx], (error) => {
-            if (error) {
-                console.log("MySQL query (INSERT INTO news (title, tags, addDate, html, docx) VALUES ('" + 
-                news.title + "', '" + news.tags + "', '" + news.addDate + "', '...', '...') finished with error: " + error.code)
+        var query = mysql.connection.query(
+            'INSERT INTO news (title, tags, addDate, html, docx) VALUES (?, ?, ?, ?, ?)',
+            [news.title, news.addDate, news.html, news.docx],
+            (error) => {
+                if (error) {
+                    console.log(
+                        "MySQL query (INSERT INTO news (title, tags, addDate, html, docx) VALUES ('" +
+                            news.title +
+                            "', '" +
+                            news.tags +
+                            "', '" +
+                            news.addDate +
+                            "', '" +
+                            html.slice(html.length - 100, html.length - 1) +
+                            "', '...') finished with error: " +
+                            error.code
+                    )
 
-                resolve(true)
-            } else {
-                console.log("MySQL query (INSERT INTO news (title, addDate, html, docx) VALUES ('" + 
-                news.title + "', '" + news.tags + "', '" + news.addDate + "', '...', '...') successfully done.")
+                    resolve(true)
+                } else {
+                    console.log(
+                        "MySQL query (INSERT INTO news (title, addDate, html, docx) VALUES ('" +
+                            news.title +
+                            "', '" +
+                            news.tags +
+                            "', '" +
+                            news.addDate +
+                            "', '...', '...') successfully done."
+                    )
 
-                resolve(false)
+                    resolve(false)
+                }
             }
-        })
+        )
     })
 }
 
 exports.selectNews = (limit, tags) => {
     return new Promise((resolve) => {
-        var query = mysql.connection.query(`SELECT * FROM news ${ tags ? `WHERE tags LIKE "%${tags}%" ` : '' }ORDER BY addDate DESC LIMIT ${limit}`, (error, rows) => {
-            if (error) {
-                console.log("MySQL query (" + query.sql + ") finished with error: " + error.code)
+        var query = mysql.connection.query(
+            `SELECT * FROM news ${
+                tags ? `WHERE tags LIKE "%${tags}%" ` : ''
+            }ORDER BY addDate DESC LIMIT ${limit}`,
+            (error, rows) => {
+                if (error) {
+                    console.log(
+                        'MySQL query (' + query.sql + ') finished with error: ' + error.code
+                    )
 
-                resolve({ error: true, data: null })
-            } else {
-                console.log("MySQL query (" + query.sql + ") successfully done.")
+                    resolve({ error: true, data: null })
+                } else {
+                    console.log('MySQL query (' + query.sql + ') successfully done.')
 
-                resolve({ error: false, data: rows })
+                    resolve({ error: false, data: rows })
+                }
             }
-        })
+        )
     })
 }
 
 exports.selectNewsWithoutImgs = (limit, tags) => {
     return new Promise((resolve) => {
-        var query = mysql.connection.query(`SELECT id, title, tags, addDate FROM news ${ tags ? `WHERE tags LIKE "%${tags}%" ` : '' }ORDER BY addDate DESC LIMIT ${limit}`, (error, rows) => {
-            if (error) {
-                console.log("MySQL query (" + query.sql + ") finished with error: " + error.code)
+        var query = mysql.connection.query(
+            `SELECT id, title, tags, addDate FROM news ${
+                tags ? `WHERE tags LIKE "%${tags}%" ` : ''
+            }ORDER BY addDate DESC LIMIT ${limit}`,
+            (error, rows) => {
+                if (error) {
+                    console.log(
+                        'MySQL query (' + query.sql + ') finished with error: ' + error.code
+                    )
 
-                resolve({ error: true, data: null })
-            } else {
-                console.log("MySQL query (" + query.sql + ") successfully done.")
+                    resolve({ error: true, data: null })
+                } else {
+                    console.log('MySQL query (' + query.sql + ') successfully done.')
 
-                resolve({ error: false, data: rows })
+                    resolve({ error: false, data: rows })
+                }
             }
-        })
+        )
     })
 }
 
 exports.selectNewsById = (id) => {
     return new Promise((resolve) => {
-        var query = mysql.connection.query("SELECT * FROM news WHERE id = " + id, (error, rows) => {
+        var query = mysql.connection.query('SELECT * FROM news WHERE id = ' + id, (error, rows) => {
             if (error) {
-                console.log("MySQL query (" + query.sql + ") finished with error: " + error.code)
+                console.log('MySQL query (' + query.sql + ') finished with error: ' + error.code)
 
                 resolve({ error: true, data: null })
             } else {
-                console.log("MySQL query (" + query.sql + ") successfully done.")
+                console.log('MySQL query (' + query.sql + ') successfully done.')
 
                 resolve({ error: false, data: rows[0] })
             }
@@ -82,38 +116,53 @@ exports.selectNewsById = (id) => {
 
 exports.updateNews = (news) => {
     return new Promise((resolve) => {
-        var values = "title = '" + news.title +
-        (news.tags != null ? "', tags = '" + news.tags : "")
-        + "', addDate = ?" +
-        (news.html != null ? ", html = '" + news.html + "'" : "") +
-        (news.docx != null ? ", docx = ?" : "")
+        var values =
+            "title = '" +
+            news.title +
+            (news.tags != null ? "', tags = '" + news.tags : '') +
+            "', addDate = ?" +
+            (news.html != null ? ", html = '" + news.html + "'" : '') +
+            (news.docx != null ? ', docx = ?' : '')
 
-        var query = mysql.connection.query("UPDATE news SET " + values + " WHERE id = " + news.id, [news.addDate, news.docx], (error, result) => {
-            if (error) {
-                // console.log("MySQL query (UPDATE news SET title = '" + 
-                // news.title + "', date = '" + news.addDate + "', html = '...', docx = '...' WHERE id = " + news.id + ") finished with error: " + error.code)
-                console.log("MySQL query (" + query.sql + ") finished with error: " + error.code)
+        var query = mysql.connection.query(
+            'UPDATE news SET ' + values + ' WHERE id = ' + news.id,
+            [news.addDate, news.docx],
+            (error, result) => {
+                if (error) {
+                    // console.log("MySQL query (UPDATE news SET title = '" +
+                    // news.title + "', date = '" + news.addDate + "', html = '...', docx = '...' WHERE id = " + news.id + ") finished with error: " + error.code)
+                    console.log(
+                        'MySQL query (' + query.sql + ') finished with error: ' + error.code
+                    )
 
-                resolve({ error: true, data: 0 })
-            } else {
-                console.log("MySQL query (UPDATE news SET title = '" + 
-                news.title + "', date = '" + news.addDate + "', html = '...', docx = '...' WHERE id = " + news.id + ") successfully done.")
+                    resolve({ error: true, data: 0 })
+                } else {
+                    console.log(
+                        "MySQL query (UPDATE news SET title = '" +
+                            news.title +
+                            "', date = '" +
+                            news.addDate +
+                            "', html = '...', docx = '...' WHERE id = " +
+                            news.id +
+                            ') successfully done.'
+                    )
 
-                resolve({ error: false, data: result.affectedRows })
+                    resolve({ error: false, data: result.affectedRows })
+                }
             }
-        })
+        )
     })
 }
 
 exports.deleteNews = (id) => {
     return new Promise((resolve) => {
-        var query = mysql.connection.query("DELETE FROM news WHERE id = " + id, (error, result) => {
+        var query = mysql.connection.query('DELETE FROM news WHERE id = ' + id, (error, result) => {
             if (error) {
-                console.log("MySQL query (" + query.sql + ") finished with error: " + error.code)
+                console.log('MySQL query (' + query.sql + ') finished with error: ' + error.code)
 
                 resolve({ error: true, data: 0 })
             } else {
-                console.log("MySQL query (" + query.sql + ") successfully done.")
+                console.log('MySQL query (' + query.sql + ') successfully done.')
 
                 resolve({ error: false, data: result.affectedRows })
             }
