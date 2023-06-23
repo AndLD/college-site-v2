27c7511174d7ef29901fbc7ece4906654433b1f5
Asura228-of <135864893+Asura228-of@users.noreply.github.com>
2023-06-23 10:39:29 +0300

    airplane2

--- resources/styles/page-up.js
+++ resources/styles/page-up.js
@@ -1,4 +1,4 @@
-window.addEventListener('scroll', function() {
+/*window.addEventListener('scroll', function() {
     var scrollButton = document.getElementById('scrollButton');
     if (window.scrollY > 0) {
       scrollButton.style.display = 'block'; // Показываем кнопку при прокрутке вниз
@@ -6,3 +6,45 @@ window.addEventListener('scroll', function() {
       scrollButton.style.display = 'none'; // Скрываем кнопку, если прокрутка вверх
     }
   });
+  window.onscroll = function() {scrollFunction()};
+
+  var isScrolling = false;
+
+  window.addEventListener("scroll", function(event) {
+    if (!isScrolling) {
+      window.requestAnimationFrame(function() {
+        scrollFunction();
+        isScrolling = false;
+      });
+      isScrolling = true;
+    }
+  });
+ */ 
+  function scrollFunction() {
+    var scrollButton = document.getElementById("scrollButton");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollButton.classList.add("show");
+    } else {
+      scrollButton.classList.remove("show");
+    }
+  }
+  
+  function scrollToTop() {
+    var scrollButton = document.getElementById("scrollButton");
+    var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    scrollButton.classList.add("flight");
+    window.requestAnimationFrame(scrollStep);
+  
+    function scrollStep() {
+      if (currentPosition > 0) {
+        currentPosition -= currentPosition / 10;
+        window.scrollTo(0, currentPosition);
+        window.requestAnimationFrame(scrollStep);
+      } else {
+        scrollButton.classList.remove("flight");
+        if (document.documentElement.scrollTop === 0) {
+          scrollButton.classList.add("hide");
+        }
+      }
+    }
+  }
