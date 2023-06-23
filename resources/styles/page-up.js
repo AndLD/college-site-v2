6c2f6a563c692cc5082fb592dd650d5cc4afd4d6
Asura228-of <135864893+Asura228-of@users.noreply.github.com>
2023-06-23 22:21:26 +0300

    Update page-up.js

--- resources/styles/page-up.js
+++ resources/styles/page-up.js
@@ -3,55 +3,53 @@
   var scrollButton = document.getElementById("scrollButton")
   scrollButton.onclick = scrollToTop
 
-var isScrolling = false;
-
-window.addEventListener("scroll", function(event) {
-  if (!isScrolling) {
-    window.requestAnimationFrame(function() {
-      scrollFunction();
-      isScrolling = false;
-    });
-    isScrolling = true;
-  }
-});
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scrollButton.classList.add("show");
-  } else {
-    scrollButton.classList.remove("show");
-  }
-}
-
-function scrollToTop() {
-  var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
-  scrollButton.classList.add("flight");
-  window.requestAnimationFrame(scrollStep);
-
-  function scrollStep() {
-    if (currentPosition > 0) {
-      currentPosition -= currentPosition / 10;
-      window.scrollTo(0, currentPosition);
-      window.requestAnimationFrame(scrollStep);
+  var isScrolling = false;
+  var isAnimating = false;
+  
+  window.addEventListener("scroll", function(event) {
+    if (!isScrolling && !isAnimating) {
+      window.requestAnimationFrame(function() {
+        scrollFunction();
+        isScrolling = false;
+      });
+      isScrolling = true;
+    }
+  });
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollButton.classList.add("show");
     } else {
-      scrollButton.classList.remove("flight");
-      if (document.documentElement.scrollTop === 0) {
-        scrollButton.classList.add("hide");
-        window.removeEventListener("wheel", preventScroll);
-      }
+      scrollButton.classList.remove("show");
     }
   }
-
-  function preventScroll(event) {
-    event.preventDefault();
-    event.stopPropagation();
-  }
-
-  window.addEventListener("wheel", preventScroll, { passive: false });
   
-  setTimeout(function() {
-    window.removeEventListener("wheel", preventScroll);
-  }, 400); // Позволяет прокрутке возобновиться через 400 миллисекунд
-}
+  function scrollToTop() {
+    var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    scrollButton.classList.add("flight");
+    isAnimating = true;
+    window.requestAnimationFrame(scrollStep);
+  
+    function scrollStep() {
+      if (currentPosition > 0) {
+        currentPosition -= currentPosition / 10;
+        window.scrollTo(0, currentPosition);
+        window.requestAnimationFrame(scrollStep);
+      } else {
+        scrollButton.classList.remove("flight");
+        if (document.documentElement.scrollTop === 0) {
+          scrollButton.classList.add("hide");
+          isAnimating = false;
+          window.removeEventListener("wheel", preventScroll);
+        }
+      }
+    }
+  
+    function preventScroll(event) {
+      event.preventDefault();
+    }
+  
+    window.addEventListener("wheel", preventScroll, { passive: false });
+  }
   
  
