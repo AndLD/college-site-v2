444339ab06df0f51d94d9c5931ba3d1f98f23adb
Asura228-of <135864893+Asura228-of@users.noreply.github.com>
2023-06-24 14:08:04 +0300

    Update page-up.js

--- resources/styles/page-up.js
+++ resources/styles/page-up.js
@@ -1,28 +1,23 @@
-  window.onscroll = scrollFunction
-  
-  var scrollButton = document.getElementById("scrollButton")
-  scrollButton.onclick = scrollToTop
-
-  var isScrolling = false;
-
-  window.addEventListener("scroll", function() {
-    if (window.pageYOffset > 100) {
-      scrollButton.classList.add("show");
-    } else {
-      scrollButton.classList.remove("show");
-    }
-  });
-  
-  function scrollToTop() {
-    scrollButton.classList.add("flight");
-    setTimeout(function() {
-      scrollButton.classList.remove("flight");
-    }, 500);
-  
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
+window.addEventListener("scroll", function() {
+  var scrollButton = document.getElementById("scrollButton");
+  if (window.pageYOffset > 100) {
+    scrollButton.classList.add("show");
+  } else {
+    scrollButton.classList.remove("show");
   }
+});
+
+function scrollToTop() {
+  var scrollButton = document.getElementById("scrollButton");
+  scrollButton.classList.add("flight");
+  setTimeout(function() {
+    scrollButton.classList.remove("flight");
+  }, 500);
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
   
  
