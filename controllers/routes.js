bd11271f8a192b8b0add01dfb68f094d5a74592f
Asura228-of <135864893+Asura228-of@users.noreply.github.com>
2023-09-18 00:04:01 +0300

    search_

--- controllers/routes.js
+++ controllers/routes.js
@@ -8,23 +8,43 @@ const articleModel = require("c:/Users/Андрей/Documents/GitHub/college-sit
 
 // Подключение к базе данных 
 const mysql = require('c:/Users/Андрей/Documents/GitHub/college-site-v2/db/mysql');
-mysql.connect('mysql://localhost/mydb', { useNewUrlParser: true });
 
-// Модель статьи
-const articles = await articleModel.find({ title: { $regex: searchTerm, $options: 'i' } });
+connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+})
+
+connection.connect((error) => {
+        if (error != null) {
+            console.log('MySQL connection error:')
+            console.log(error.code)
+        } else {
+            console.log('MySQL successfully connected.')
+        }
+    })
 
 // Роут для поиска статей
-server.get('/search', async (req, res) => {
-    const query = req.query.query; // Получаем строку поиска из запроса
-    try {
-        // Ищем статьи, в названии которых есть введенная строка
-        const articles = await Article.find({ title: { $regex: query, $options: 'i' } });
-        res.json(articles);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Ошибка сервера' });
-    }
-});
+// Роут для выполнения поиска по названиям статей
+server.get('/search', (req, res) => {
+    const searchString = req.query.q; // Получаем строку поиска из запроса
+  
+    // SQL-запрос для выполнения поиска
+    const sqlQuery = `SELECT * FROM articles WHERE title LIKE '%${searchString}%'`;
+  
+    // Выполняем SQL-запрос к базе данных
+    connection.query(sqlQuery, (err, results) => {
+      if (err) {
+        console.error('Ошибка выполнения SQL-запроса:', err);
+        res.status(500).json({ error: 'Произошла ошибка на сервере' });
+      } else {
+        // Отправляем результаты поиска в формате JSON
+        res.json(results);
+      }
+    });
+  });
 
 // Запускаем сервер
 const PORT = process.env.PORT
